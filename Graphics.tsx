7bac11fcac0627f2ff0cf085d57682ca600bad3a
bruno <bruno.costa@cinnecta.com>
2020-04-22 10:53:17 -0300

    Migração do expo para react-native bare

--- Graphics.tsx
+++ Graphics.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { View, Text } from "react-native";
+import { View, Text } from 'react-native';
 
 export default function Graphics({ navigation }: any) {
     return (
